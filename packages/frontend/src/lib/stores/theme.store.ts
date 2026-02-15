e1dee05db076c32f40057e8622a006846ce76380
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-02-15 15:31:36 +0100

    Delete packages/frontend directory

--- packages/frontend/src/lib/stores/theme.store.ts
+++
@@ -1,5 +0,0 @@
-import { persisted } from 'svelte-persisted-store';
-
-type Theme = 'light' | 'dark' | 'system';
-
-export const theme = persisted<Theme>('theme', 'system');
--- packages/frontend/src/routes/signin/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load = (async (event) => {
-	const { locals } = event;
-	if (locals.user) {
-		throw redirect(307, '/dashboard');
-	}
-}) satisfies PageServerLoad;
